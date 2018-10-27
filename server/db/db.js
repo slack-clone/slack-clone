663f6f39752404b5d195ae5ac82e8b66624afd0a
elizabethf <farrier.elizabeth@gmail.com>
2018-10-27 16:40:43 -0400

    update package.json

--- server/db/db.js
+++ server/db/db.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
 
-const db = new Sequelize('postgres://localhost:5432/slack-clone', { logging: false });
+const db = new Sequelize('postgres://localhost:5432/slack', { logging: false });
 
 module.exports = db;
