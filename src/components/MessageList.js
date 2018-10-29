7d0fc81a3ec9c50afea8d6674c4a680dad395fe8
Elis Dervishi <elis.derv@gmail.com>
2018-10-29 11:08:58 -0400

    Fetch messages from server

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -3,6 +3,17 @@ import React, { Component } from 'react';
 
 export default class MessageList extends Component {
 
+  constructor () {
+    super();
+    this.state = { messages: [] };
+  }
+
+  componentDidMount () {
+    axios.get('/api/messages')
+      .then(res => res.data)
+      .then(messages => this.setState({ messages }));
+  }
+  
   render () {
     return (
       <div>
