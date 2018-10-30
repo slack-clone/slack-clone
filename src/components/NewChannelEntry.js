5b1d4ee09a9b4ceac64424a9875cc1d2c488d1d3
Elis Dervishi <elis.derv@gmail.com>
2018-10-30 13:22:28 -0400

    Channel name form

--- src/components/NewChannelEntry.js
+++ src/components/NewChannelEntry.js
@@ -5,8 +5,19 @@ export default class NewChannelEntry extends Component {
 
   render () {
     return (
-      <div>
-      </div>
+      <form>
+        <div>
+          <label>Create a Channel</label>
+          <input
+            type="text"
+            name="channelName"
+            placeholder="Enter channel name"
+          />
+        </div>
+        <div>
+          <button type="submit">Create Channel</button>
+        </div>
+      </form>
     );
   }
 }
