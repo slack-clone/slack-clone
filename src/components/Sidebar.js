f1651167ccba8500b9bb9c1d4aac98cea9374982
elizabethf <farrier.elizabeth@gmail.com>
2018-11-01 20:51:38 -0400

    channellist, sidebar components

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
+import ChannelList from './ChannelList';
 
-
-export default class SideBar extends Component {
+export default class Sidebar extends Component {
 
   render () {
     return (
-      <div>
-      </div>
+      <section >
+        <div>
+          <h3 href="#">
+            <div>Slack</div>
+          </h3>
+        </div>
+        <h5>channels</h5>
+        <ChannelList />
+      </section>
     );
   }
 }
