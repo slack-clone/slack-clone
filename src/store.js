a5f2e348ee93cdddf6f75f1d7bc486d2fd6d1d1f
Elis Dervishi <elis.derv@gmail.com>
2018-11-04 07:43:00 -0500

    init redux store with get messages

---
+++ src/store.js
@@ -0,0 +1,26 @@
+import { createStore } from 'redux';
+
+const GOT_MESSAGES_FROM_SERVER = 'GOT_MESSAGES_FROM_SERVER';
+
+export function gotMessagesFromServer(messages) {
+  return {
+    type: GOT_MESSAGES_FROM_SERVER,
+    messages
+  };
+}
+
+const initialState = {
+  messages: []
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case GOT_MESSAGES_FROM_SERVER:
+       return Object.assign({}, state, { messages: action.messages });
+    default:
+       return state;
+  }
+}
+
+const store = createStore(reducer);
+export default store;
