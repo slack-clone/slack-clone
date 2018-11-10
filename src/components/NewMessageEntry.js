7c338f4727b46bbb91463233666e02c3a68608b5
elizabethf <farrier.elizabeth@gmail.com>
2018-11-10 10:25:41 -0500

    subscribe new message entry to store

--- src/components/NewMessageEntry.js
+++ src/components/NewMessageEntry.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
+import store from '../store';
 
 
 export default class NewMessageEntry extends Component {
+  constructor() {
+    super();
+    this.state = store.getState();
+  }
+
+  componentDidMount() {
+    this.unsubscribe = store.subscribe(() => this.setState(store.getState()));
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
 
   render () {
     return (
